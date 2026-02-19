6d6732654dee59b9f5fe529a18b394065bcfa36c
yeshCEO786 <rajanayeshaswiniceo78631888@gmail.com>
2026-02-19 23:32:14 +0530

    Update Landing.jsx

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
-import "..styles/Landing.css";
+import "../styles/Landing.css";
 
 function Landing() {
   const navigate = useNavigate();
