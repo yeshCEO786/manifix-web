94796839ff899808982f6093aee7fefece5407bb
yeshCEO786 <rajanayeshaswiniceo78631888@gmail.com>
2026-02-24 01:06:30 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useApp } from "../context/AppContext";
-import { supabase } from "../services/supabase";
+import  supabase  from "../services/supabase";
 
 const Dashboard = () => {
   const navigate = useNavigate();
