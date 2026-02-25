86bb126806c5fd80fb67dd99b0ea998044f90184
yeshCEO786 <rajanayeshaswiniceo78631888@gmail.com>
2026-02-25 10:41:24 +0530

    Update index.ts

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,34 +1,9 @@
-export const icons = {
-  mic: require("./microphone.png"),
-  stop: require("./stop.png"),
+import starFilled from "./star-filled-666.png";
+import target from "./target.png";
+import magic16 from "./magic16.png";
+
+export const Icons = {
+  starFilled,
+  target,
+  magic16,
 };
-const PngIcons = {
-  // Core
-  manifix: require('./ManifiX_icon.png'),
-  home: require('./home_icon.png'),
-  feed: require('./feed_icon.png'),
-  profile: require('./profile_icon.png'),
-  settings: require('./setting_icon.png'),
-
-  // Chat
-  chat: require('./chat_icon.png'),
-  send: require('./send.png'),
-  microphone: require('./microphone_icon.png'),
-  stop: require('./stop.png'),
-  warning: require('./warning.png'),
-
-  // Auth / UI
-  check: require('./check.png'),
-  eye: require('./eye.png'),
-  eyeOff: require('./eye-off.png'),
-
-  // Magic16
-  magic16: require('./magic16.png'),
-  target: require('./target.png'),
-
-  // Rating
-  starEmpty: require('./star-empty.png'),
-  starFilled: require('./star-filled-666.png'),
-};
-
-export default PngIcons;
