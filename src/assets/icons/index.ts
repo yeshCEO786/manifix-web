28d5b1bac37f44757e408c97abb8af04ca8e24d0
yeshCEO786 <rajanayeshaswiniceo78631888@gmail.com>
2026-02-25 10:52:07 +0530

    Update index.ts

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,8 +2,13 @@ import starFilled from "./star-filled-666.png";
 import target from "./target.png";
 import magic16 from "./magic16.png";
 
+import like from "./like.png";
+import share from "./share.png";
+
 export const Icons = {
   starFilled,
   target,
   magic16,
+  like,
+  share,
 };
