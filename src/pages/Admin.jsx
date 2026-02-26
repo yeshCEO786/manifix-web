9a9a0aae3d885668bc20c97d50c0fafdf5013d48
yeshCEO786 <rajanayeshaswiniceo78631888@gmail.com>
2026-02-26 13:14:09 +0530

    Update Admin.jsx

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { supabase } from "../lib/supabase";
+import { supabase } from "../service/supabase";
 import { useApp } from "../context/AppContext";
 
 const Admin = () => {
