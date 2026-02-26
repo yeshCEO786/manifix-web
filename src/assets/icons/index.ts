524e086ec39fef08b095980303b7f946db110118
yeshCEO786 <rajanayeshaswiniceo78631888@gmail.com>
2026-02-26 13:40:52 +0530

    Update index.ts

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,15 +1,35 @@
-export { default as ChatIcon } from "./chat_icon.png";
-export { default as CheckIcon } from "./check.png";
-export { default as EyeIcon } from "./eye.png";
-export { default as EyeOffIcon } from "./eye-off.png";
-export { default as FeedIcon } from "./feed_icon.png";
-export { default as HomeIcon } from "./home_icon.png";
-export { default as Magic16Icon } from "./magic16.png";
-export { default as ProfileIcon } from "./profile_icon.png";
-export { default as SendIcon } from "./send.png";
-export { default as SettingIcon } from "./setting_icon.png";
-export { default as StarEmptyIcon } from "./star-empty.png";
-export { default as StarFilled666Icon } from "./star-filled-666.png";
-export { default as StopIcon } from "./stop.png";
-export { default as TargetIcon } from "./target.png";
-export { default as WarningIcon } from "./warning.png";
+import ChatIcon from "./chat_icon.png";
+import CheckIcon from "./check.png";
+import EyeIcon from "./eye.png";
+import EyeOffIcon from "./eye-off.png";
+import FeedIcon from "./feed_icon.png";
+import HomeIcon from "./home_icon.png";
+import Magic16Icon from "./magic16.png";
+import ProfileIcon from "./profile_icon.png";
+import SendIcon from "./send.png";
+import SettingIcon from "./setting_icon.png";
+import StarEmptyIcon from "./star-empty.png";
+import StarFilled666Icon from "./star-filled-666.png";
+import StopIcon from "./stop.png";
+import TargetIcon from "./target.png";
+import WarningIcon from "./warning.png";
+
+const Icons = {
+  ChatIcon,
+  CheckIcon,
+  EyeIcon,
+  EyeOffIcon,
+  FeedIcon,
+  HomeIcon,
+  Magic16Icon,
+  ProfileIcon,
+  SendIcon,
+  SettingIcon,
+  StarEmptyIcon,
+  StarFilled666Icon,
+  StopIcon,
+  TargetIcon,
+  WarningIcon,
+};
+
+export default Icons;
