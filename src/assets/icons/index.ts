da2da9108d589289f27878729ec37a16b8de6bdb
yeshCEO786 <rajanayeshaswiniceo78631888@gmail.com>
2026-02-26 13:27:43 +0530

    Update index.ts

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,82 +1,16 @@
-/* ============================
-   ManifiX Global Icon System
-   100% Vite Compatible
-============================ */
-
-/* ========= SVG Icons ========= */
-
-import gpt from "./gpt.svg";
-
-/* ========= Core PNG Icons ========= */
-import manifix from "./ManifiX_icon.png";
-import home from "./home_icon.png";
-import feed from "./feed_icon.png";
-import profile from "./profile_icon.png";
-import settings from "./setting_icon.png";
-
-/* ========= Chat Icons ========= */
-import chat from "./chat_icon.png";
-import send from "./send.png";
-import microphone from "./microphone_icon.png";
-import stop from "./stop.png";
-import warning from "./warning.png";
-
-/* ========= Auth / UI ========= */
-import check from "./check.png";
-import eye from "./eye.png";
-import eyeOff from "./eye-off.png";
-
-/* ========= Magic16 ========= */
-import magic16 from "./magic16.png";
-import target from "./target.png";
-
-/* ========= Rating ========= */
-import starEmpty from "./star-empty.png";
-import starFilled from "./star-filled-666.png";
-
-/* ========= Voice ========= */
-import mic from "./microphone.png";
-import stopVoice from "./stop.png";
-
-/* ============================
-   Export Everything
-============================ */
-
-const Icons = {
-  // SVG
-  dashboard,
-  gpt,
-
-  // Core
-  manifix,
-  home,
-  feed,
-  profile,
-  settings,
-
-  // Chat
-  chat,
-  send,
-  microphone,
-  stop,
-  warning,
-
-  // Auth
-  check,
-  eye,
-  eyeOff,
-
-  // Magic16
-  magic16,
-  target,
-
-  // Rating
-  starEmpty,
-  starFilled,
-
-  // Voice
-  mic,
-  stopVoice,
-};
-
-export default Icons;
+export { default as ChatIcon } from "./chat_icon.png";
+export { default as CheckIcon } from "./check.png";
+export { default as EyeIcon } from "./eye.png";
+export { default as EyeOffIcon } from "./eye-off.png";
+export { default as FeedIcon } from "./feed_icon.png";
+export { default as HomeIcon } from "./home_icon.png";
+export { default as Magic16Icon } from "./magic16.png";
+export { default as MicrosoftIcon } from "./microsoft.png";
+export { default as ProfileIcon } from "./profile_icon.png";
+export { default as SendIcon } from "./send.png";
+export { default as SettingIcon } from "./setting_icon.png";
+export { default as StarEmptyIcon } from "./star-empty.png";
+export { default as StarFilled666Icon } from "./star-filled-666.png";
+export { default as StopIcon } from "./stop.png";
+export { default as TargetIcon } from "./target.png";
+export { default as WarningIcon } from "./warning.png";
