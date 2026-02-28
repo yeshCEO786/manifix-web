26d0e1a14b49defb422db862a0fc0343ca92d206
yeshCEO786 <rajanayeshaswiniceo78631888@gmail.com>
2026-02-28 18:37:15 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AppProvider } from "./context/AppContext"; // adjust path if needed
+import { AppProvider } from "./context/AppContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
       <AppProvider>
         <App />
       </AppProvider>
-    </BrowserRouter>
   </React.StrictMode>
 );
