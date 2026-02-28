bc8d8f92fc6d1f98451cd4d80359c207deb2cfbb
yeshCEO786 <rajanayeshaswiniceo78631888@gmail.com>
2026-02-28 19:01:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { AppProvider } from "./context/AppContext";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-      <AppProvider>
+    <AppProvider>
+      <BrowserRouter>
         <App />
-      </AppProvider>
+      </BrowserRouter>
+    </AppProvider>
   </React.StrictMode>
 );
