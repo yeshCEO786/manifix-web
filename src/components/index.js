3dcfe24aa12ae3e8fe5f1bcfc42f68db8f4c6da9
yeshCEO786 <rajanayeshaswiniceo78631888@gmail.com>
2026-03-05 09:16:35 +0530

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,24 @@
+// Layout
+export { default as MainLayout } from "./Layout/MainLayout";
+export { default as MobileLayout } from "./Layout/MobileLayout";
+export { default as TopBar } from "./Layout/TopBar";
+export { default as BottomNav } from "./Layout/BottomNav";
+
+// Chat (inside Chat folder)
+export { default as ChatBox } from "./Chat/ChatBox";
+export { default as ChatInput } from "./Chat/ChatInput";
+export { default as ChatMessage } from "./Chat/ChatMessage";
+// Magic16
+export { default as Magic16Controls } from "./Magic16/Magic16Controls";
+export { default as Magic16Timer } from "./Magic16/Magic16Timer";
+
+// Vibe
+export { default as ProfileCard } from "./Profile/ProfileCard";
+
+// Camera / Vision
+export { default as CameraView } from "./Camera/CameraView";
+export { default as CameraStats } from "./Camera/CameraStats";
+export { default as EmotionOverlay } from "./Camera/EmotionOverlay";
+export { default as VisionStats } from "./Camera/VisionStats";
+export { default as Modal } from "./Modal";
+
